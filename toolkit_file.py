00adabd35010a86220fafceed43ce05585ac6757
duchenpaul <duchenpaul@gmail.com>
2018-06-10 18:04:20 +0800

    Add func get_file_list

--- toolkit_file.py
+++ toolkit_file.py
@@ -20,6 +20,13 @@ def line_prepender(filename, line):
 		f.seek(0, 0)
 		f.write(line.rstrip('\r\n') + '\n' + content)
 
+def get_file_list(dirname):
+	file_list = []
+	for i in os.walk(dirname):
+		if not i[1]:
+			for j in i[2]:
+				file_list.append(''.join(str(i[0]) + os.sep + j))
+	return file_list
 
 def purge_folder(folder):
 	filelist = [ f for f in os.listdir(folder) ] #if f.endswith(".bak") ]
@@ -29,4 +36,4 @@ def purge_folder(folder):
 		# os.remove(os.path.join(folder, f))
 
 if __name__ == '__main__':
-	pass
+	print(get_file_list('E:\\'))
