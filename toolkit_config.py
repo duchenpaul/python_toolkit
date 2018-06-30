a887efb02d04a2f0e902d749aed4b69f6eb18911
duchenpaul <duchenpaul@gmail.com>
2018-06-30 20:20:18 +0800

    Parameterize the config file

--- toolkit_config.py
+++ toolkit_config.py
@@ -7,11 +7,11 @@ import configparser, os.path
 config_file = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'config.ini'
 
 
-def read_config_mail():
+def read_config_mail(configFile=config_file):
 	try:
-		print('Read config file: ' + config_file )
+		print('Read config file: ' + configFile )
 		configRead = configparser.ConfigParser()
-		configRead.read("config.ini")
+		configRead.read(configFile)
 
 		sectionName = 'mail'
 		options = [ i.upper() for i in configRead.options(sectionName) ]
@@ -28,9 +28,9 @@ def read_config_mail():
 
 def read_config_mysql():
 	try:
-		print('Read config file: ' + config_file )
+		print('Read config file: ' + configFile )
 		configRead = configparser.ConfigParser()
-		configRead.read(config_file)
+		configRead.read(configFile)
 		config = {
 			'host': configRead['datebase']['host'],
 			'port': int(configRead['datebase']['port']),
@@ -46,5 +46,5 @@ def read_config_mysql():
 	return config
 
 if __name__ == '__main__':
-	config = read_config_mail()
+	config = read_config_mail('config_test.ini')
 	print(config)
