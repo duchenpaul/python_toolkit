d0bb16aade119be4b7869751c61433ae800d982c
chdu <duchenpaul@gmail.com>
2018-07-29 23:35:52 +0800

    Add fetching the content of a mail

--- toolkit_outlook.py
+++ toolkit_outlook.py
@@ -91,6 +91,19 @@ class _Outlook():
 
         print("Purge done")
 
+    def fetch_mail(self, folderName = 'INBOX', subject = 'keywords'):
+        '''
+        Fetch the subject of mail in a folder
+        '''
+        if folderName.upper() == 'INBOX':
+            # Fetch inbox
+            folder = self.outlookAPI.GetDefaultFolder(6)
+        else:
+            folder = self.inbox.Folders[folderName]
+
+        # folder = self.inbox.Folders[folderName]
+        messages = folder.Items
+        return [message.HTMLBody for message in messages if message.Subject == subject]
 
 outlook = _Outlook()
 
