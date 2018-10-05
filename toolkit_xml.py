ddcf9f7907f80530a48ab0e406561271e4f4ae71
chdu <duchenpaul@gmail.com>
2018-10-05 21:06:56 +0800

    Add toolkit_xml

---
+++ toolkit_xml.py
@@ -0,0 +1,29 @@
+from lxml import etree
+
+import xmltodict
+import json
+
+
+def xml_minify(xml_str):
+    parser = etree.XMLParser(remove_blank_text=True)
+    xml_str = bytes(bytearray(xml_str, encoding='utf-8'))
+    elem = etree.XML(xml_str, parser=parser)
+    return etree.tostring(elem, xml_declaration=True, encoding='utf-8').decode().replace('\n', '')
+
+
+def xml2json(xmlString):
+    '''Return json object'''
+    jsonString = json.dumps(xmltodict.parse(xmlString), indent=4)
+    xml_in_json = json.loads(jsonString)
+    return xml_in_json
+
+
+if __name__ == '__main__':
+    xmlsample = 'api_export/getListformated_sample.xml'
+
+    xmlString = open(xmlsample, encoding = 'utf-8').read()
+    xmlsamplejson = xml2json(xmlString)
+    update_date = xmlsamplejson['feed']['modified']
+
+    # with open('api_export/test.json', 'w') as f:
+    #     f.write(jsonString)
