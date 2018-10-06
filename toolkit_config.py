63e812df17978e9945f6f3fa25bcaf224d072a7c
chdu <duchenpaul@gmail.com>
2018-10-06 08:30:26 +0800

    Add read_config_general

--- toolkit_config.py
+++ toolkit_config.py
@@ -7,6 +7,26 @@ import configparser, os.path
 config_file = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'config.ini'
 
 
+def read_config_general(configFile=config_file):
+    try:
+        print('Read config file: ' + configFile)
+        configRead = configparser.ConfigParser()
+        configRead.read(configFile)
+
+        config = {}
+        for sectionName in configRead.sections():
+            config[sectionName] = {}
+            options = [i.upper() for i in configRead.options(sectionName)]
+            for j in options:
+                config[sectionName][j] = configRead[sectionName][j]
+
+    except Exception as e:
+        print("Error read config file, check config.ini")
+        print(e)
+        raise
+    return config
+
+
 def read_config_mail(configFile=config_file):
     try:
         print('Read config file: ' + configFile )
